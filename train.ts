45d16826aa753754101b54e42c613336742935ae
ali <ibrakhimov7747a@gmail.com>
2024-05-16 15:57:59 +0900

    feat: ZM TASK done

--- train.ts
+++ train.ts
@@ -1,10 +1,15 @@
-/* ZL-TASK:
+/*ZM-TASK:
 
-Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
-MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
+Shunday function yozing, u function parametrga berilgan raqamlarni orqasiga ogirib qaytarsin.
+MASALAN: reverseInteger(123456789) return 987654321
+
 */
-function stringToKebab(inputString: string): any {
-	const kebabstring = inputString.toLowerCase().replace(/\s+/g, '-');
-	return kebabstring;
+
+function reverseInteger(num: number): number {
+	const numStr: string = num.toString();
+
+	const reversedStr: string = numStr.split('').reverse().join('');
+	const reversedNum: number = parseInt(reversedStr);
+	return reversedNum;
 }
-console.log(stringToKebab('I love Kebab'));
+console.log(reverseInteger(123456789));
