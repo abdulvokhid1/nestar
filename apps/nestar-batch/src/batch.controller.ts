0997ee1a7afca1e38c085c3b867f84bbb676ef64
ali <ibrakhimov7747a@gmail.com>
2024-06-04 17:05:47 +0900

    feat:develop job Scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -10,47 +10,46 @@ export class BatchController {
 
 	@Timeout(1000)
 	handleTimeout() {
-		this.logger.debug('BATCH SERVER READY');
+		this.logger.debug('BATCH SERVER READY!');
 	}
 
-	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	@Cron('00 00 01 * * *', { name: BATCH_ROLLBACK })
 	public async batchRollback() {
 		try {
 			this.logger['context'] = BATCH_ROLLBACK;
-			this.logger.debug('EXECUTED');
+			this.logger.debug('EXECUTED!');
 			await this.batchService.batchRollback();
 		} catch (err) {
-			this.logger.log(err);
+			this.logger.error(err);
 		}
 	}
 
-	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
-	public async batchPropertiesk() {
+	@Cron('20 00 01 * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
 		try {
 			this.logger['context'] = BATCH_TOP_PROPERTIES;
-			this.logger.debug('EXECUTED');
-			await this.batchService.batchProperties();
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopProperties();
 		} catch (err) {
-			this.logger.log(err);
+			this.logger.error(err);
 		}
 	}
-	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
-	public async batchAgents() {
-		this.logger['context'] = BATCH_TOP_AGENTS;
-		this.logger.debug('EXECUTED');
-		await this.batchService.batchAgents();
-		try {
-		} catch (err) {
-			this.logger.log(err);
-		}
-	}
-	/* 
-	@Interval(1000)
-	handleInterval() {
-		this.logger.debug('INTERVAL TEST');
-	}
- */
 
+	@Cron('40 00 01 * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	// @Interval(1000)
+	// handleInterval() {
+	// 	this.logger.debug('INTERVAL TEST');
+	// }
 	@Get()
 	getHello(): string {
 		return this.batchService.getHello();
